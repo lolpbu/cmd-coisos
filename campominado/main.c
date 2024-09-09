67f81b76b32912246a5a27602cd4eaa31df78aa4
lolpbu <viniciusidalino@hotmail.com>
2024-09-09 13:50:14 -0300

    oi

--- campominado/main.c
+++ campominado/main.c
@@ -1,62 +1,116 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define W 8
 #define H 4
 
 int main() {
+  char tiles[H][W];
+  tiles[1][1] = 'B';
+  tiles[1][2] = 'B';
+  tiles[3][6] = 'B';
+  tiles[2][6] = 'B';
+  tiles[0][7] = 'B';
+  tiles[3][5] = 'B';
 
- char tiles[H][W];
- tiles[1][1] = 'B';
- tiles[1][2] = 'B';
- tiles[3][6] = 'B';
- tiles[2][6] = 'B';
- tiles[0][7] = 'B';
- tiles[3][5] = 'B';
+  for(int i=0; i<H; i++)
+    for(int j=0; j<W; j++)
+      if(tiles[i][j] != 'B')
+        tiles[i][j] = '#';
 
- for(int i=0; i<H; i++) {
-  for(int j=0; j<W; j++) {
-   if(tiles[i][j] == 'B')
-    continue;
-   
-   bool _a=0, _b=0, _c=0, _d=0, _e=0, _f=0, _g=0, _h=0;
-   bool u = i == 0;
-   bool d = i == H - 1;
-   bool l = j == 0;
-   bool r = j == W - 1;
-   //_a _b _c
-   //_d    _e
-   //_f _g _h
-   if(!u && !l)
-    _a = tiles[i-1][j-1] == 'B';
-   if(!u)
-    _b = tiles[i-1][j] == 'B';
-   if(!u && !r)
-    _c = tiles[i-1][j+1] == 'B';
-   if(!l)
-    _d = tiles[i][j-1] == 'B';
-   if(!r)
-    _e = tiles[i][j+1] == 'B';
-   if(!d && !l)
-    _f = tiles[i+1][j-1] == 'B';
-   if(!d)
-    _g = tiles[i+1][j] == 'B';
-   if(!d && !r)
-    _h = tiles[i+1][j+1] == 'B';
-   
-   int total = _a+_b+_c+_d+_e+_f+_g+_h;
-   if(total != 0)
-    tiles[i][j] = total + '0';
-   else
-    tiles[i][j] = '.';
+  bool exit = false;
+  int input_col, input_row;
+
+  while (!exit) {
+    system("CLS");
+
+    for (int i = 0; i < H; i++) {
+      for (int j = 0; j < W; j++)
+        printf("%c", tiles[i][j]);
+      printf("\n");
+    }
+
+    printf("input_col (0-%d): ", H);
+    scanf("%d", &input_col);
+    printf("input_row (0-%d): ", W);
+    scanf("%d", &input_row);
+
+    bool _a=0, _b=0, _c=0, _d=0, _e=0, _f=0, _g=0, _h=0;
+    bool u = input_col == 0;
+    bool d = input_col == H - 1;
+    bool l = input_row == 0;
+    bool r = input_row == W - 1;
+    if(!u && !l)
+      _a = tiles[input_col-1][input_row-1] == 'B';
+    if(!u)
+      _b = tiles[input_col-1][input_row] == 'B';
+    if(!u && !r)
+      _c = tiles[input_col-1][input_row+1] == 'B';
+    if(!l)
+      _d = tiles[input_col][input_row-1] == 'B';
+    if(!r)
+      _e = tiles[input_col][input_row+1] == 'B';
+    if(!d && !l)
+      _f = tiles[input_col+1][input_row-1] == 'B';
+    if(!d)
+      _g = tiles[input_col+1][input_row] == 'B';
+    if(!d && !r)
+      _h = tiles[input_col+1][input_row+1] == 'B';
+
+    int total = _a+_b+_c+_d+_e+_f+_g+_h;
+    if(total != 0)
+      tiles[input_col][input_row] = total + '0';
+    else
+      tiles[input_col][input_row] = '.';
   }
- }
+  //_a _b _c
+  //_d    _e
+  //_f _g _h
 
- for(int i=0; i<H; i++) {
-  for(int j=0; j<W; j++)
-   printf("%c", tiles[i][j]);
-  printf("\n");
- }
+  //  for(int i=0; i<H; i++) {
+  //   for(int j=0; j<W; j++) {
+  //    if(tiles[i][j] == 'B')
+  //     continue;
 
- return 0;
+  //    bool _a=0, _b=0, _c=0, _d=0, _e=0, _f=0, _g=0, _h=0;
+  //    bool u = i == 0;
+  //    bool d = i == H - 1;
+  //    bool l = j == 0;
+  //    bool r = j == W - 1;
+  //    //_a _b _c
+  //    //_d    _e
+  //    //_f _g _h
+  //    if(!u && !l)
+  //     _a = tiles[i-1][j-1] == 'B';
+  //    if(!u)
+  //     _b = tiles[i-1][j] == 'B';
+  //    if(!u && !r)
+  //     _c = tiles[i-1][j+1] == 'B';
+  //    if(!l)
+  //     _d = tiles[i][j-1] == 'B';
+  //    if(!r)
+  //     _e = tiles[i][j+1] == 'B';
+  //    if(!d && !l)
+  //     _f = tiles[i+1][j-1] == 'B';
+  //    if(!d)
+  //     _g = tiles[i+1][j] == 'B';
+  //    if(!d && !r)
+  //     _h = tiles[i+1][j+1] == 'B';
+
+  //    int total = _a+_b+_c+_d+_e+_f+_g+_h;
+  //    if(total != 0)
+  //     tiles[i][j] = total + '0';
+  //    else
+  //     tiles[i][j] = '.';
+  //   }
+  //  }
+
+  //  for(int i=0; i<H; i++) {
+  //   for(int j=0; j<W; j++)
+  //    printf("%c", tiles[i][j]);
+  //   printf("\n");
+  //  }
+
+  return 0;
 }
---
+++ campominado/make.bat
@@ -0,0 +1,71 @@
+@ECHO OFF
+SETLOCAL
+
+SET EXE="coiso.exe"
+SET SRCDIR="."
+SET INCDIR="include"
+SET BINDIR="out"
+SET EXT="c"
+SET CC="gcc"
+SET OBJS="%BINDIR%/*.o"
+
+IF "%1"=="create" GOTO :CREATE 
+IF "%1"=="clean" GOTO :CLEAN
+IF "%1"=="help" GOTO :HELP
+IF "%1"=="file" GOTO :FILE
+GOTO :MAKE 
+
+
+
+:HELP
+ECHO make        -^> compile
+ECHO make run    -^> compile ^& run
+ECHO make create -^> create bin, src ^& include dirs
+ECHO make clean  -^> clean bin dir
+ECHO make help   -^> ...
+GOTO :EOF
+
+
+
+:MAKE 
+REM /p = path to search
+REM /m = search mask (* wildcard)
+REM /s = recurse into subfolders
+REM /c = command to each file found 
+FORFILES /P %SRCDIR% /M *.%EXT% /S /C "CMD /C ECHO compilando arquivo @file && %CC% -I %INCDIR% -c @relpath -o %BINDIR%\\@FNAME.o"
+ECHO linkando arquivos em %EXE%
+%CC% %OBJS% -o %EXE%
+ECHO pronto :3
+
+IF "%1"=="run" (
+    CLS
+    %EXE%
+)
+
+GOTO :EOF
+
+
+
+:FILE
+REM compilando arquivo %2
+REM %CC% -I %INCDIR% -c %2 -o %BINDIR%\%2
+FORFILES /P %SRCDIR% /M %2 /S /C "CMD /C ECHO compilando arquivo @file && %CC% -I %INCDIR% -c @relpath -o %BINDIR%\\@FNAME.o"
+ECHO linkando arquivos em %EXE%
+%CC% %OBJS% -o %EXE%
+ECHO pronto :3
+GOTO :EOF
+
+
+:CREATE
+MKDIR %INCDIR% %BINDIR% %SRCDIR%
+GOTO :EOF
+
+
+
+:CLEAN
+DEL /Q /S *.o *.exe %BINDIR%\*
+
+
+
+:EOF
+ENDLOCAL
